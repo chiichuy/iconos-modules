3a91d8f86e05739bf3acc9e4c3ceb143a486473e
Jesus Amador <chullito999@gmail.com>
2018-03-06 13:38:08 -0800

    se agrego scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/custom.css'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 //
 // ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 
 
-export {default as Button } from './components/buttons/Button'
-export {default as ButtonSuccess } from './components/buttons/ButtonSuccess'
+import './css/custom.scss'
+
+
+// export {default as Button } from './components/buttons/Button'
+// export {default as ButtonSuccess } from './components/buttons/ButtonSuccess'
+
+
+ReactDOM.render((
+    <div>gola </div>
+), document.getElementById('root'));
+registerServiceWorker();
